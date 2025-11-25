8aeb638e76d471461120521b6664aa44c1594257
dnson384 <doannhuson2004@gmail.com>
2025-11-25 11:31:40 +0700

    feat: API - Create Course Learn Question

--- app/presentation/schemas/course_schema.py
+++ app/presentation/schemas/course_schema.py
@@ -1,12 +1,37 @@
 from pydantic import BaseModel, ConfigDict
 from uuid import UUID
+from typing import List, TypedDict
 
 
 class CourseOutput(BaseModel):
     course_id: UUID
     course_name: str
+    author_avatar_url: str
     author_username: str
     author_role: str
     num_of_terms: int
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class CourseDetailOutput(BaseModel):
+    course_detail_id: UUID
+    term: str
+    definition: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CourseWithDetailsOutput(TypedDict):
+    course: CourseOutput
+    course_detail: List[CourseDetailOutput]
+
+
+class LearnQuestionOutput(TypedDict):
+    question: CourseDetailOutput
+    options: List[CourseDetailOutput]
+
+
+class CourseLearnQuestionOutput(TypedDict):
+    course: CourseOutput
+    questions: List[LearnQuestionOutput]
